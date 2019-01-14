638451671f281ef3bdfa56584ed44aac8bb2ed31
Dimitrije <dimitrije_jankovic@outlook.com>
2019-01-14 09:58:09 +0100

    app env

---
+++ build/precache-manifest.35763950c7c1dd73ce03285788d34299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b27f67e9dcd4aaa530ff",
+    "url": "/static/js/main.b27f67e9.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b27f67e9dcd4aaa530ff",
+    "url": "/static/css/main.da5f5657.chunk.css"
+  },
+  {
+    "revision": "cfd9aaa755d82732c56cc9329e32b6c6",
+    "url": "/index.html"
+  }
+];
