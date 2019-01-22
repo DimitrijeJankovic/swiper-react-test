13aa7023ff8e1e3055fe5299769eb1de04f7a1fb
Dimitrije <dimitrije_jankovic@outlook.com>
2019-01-22 15:08:48 +0100

    css style fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,35 +10,39 @@ class Contact extends Component {
     }
 
     return(
-      <div className="contact-page">
-        <div className="overlay"></div>
-        <div className="bg" style={image}></div>
-        <div className="contact">
-            <h2>Our Contacts</h2>
-            <p>Praesent nec leo venenatis elit semper aliquet id ac enim. Maecenas nec mi leo. 
-                Etiam venenatis ut dui non hendrerit. Integer dictum, diam vitae blandit accumsan, 
-                dolor odio tempus arcu, vel ultrices nisi nibh vitae ligula.
-            </p>
-            <div className="contact-details">
-                <ul>
-                    <li><span>Mail:</span>dsadsadasda</li>
-                    <li><span>Phone:</span>445656446464</li>
-                </ul>
+        <div className="row">
+            <div className="contact-page col-md-12">
+                    <div className="overlay"></div>
+                    <div className="bg" style={image}></div>
+                <div className="test">
+                    <div className="contact">
+                        <h2>Our Contacts</h2>
+                        <p>Praesent nec leo venenatis elit semper aliquet id ac enim. Maecenas nec mi leo. 
+                            Etiam venenatis ut dui non hendrerit. Integer dictum, diam vitae blandit accumsan, 
+                            dolor odio tempus arcu, vel ultrices nisi nibh vitae ligula.
+                        </p>
+                        <div className="contact-details">
+                            <ul>
+                                <li><span>Mail:</span>dsadsadasda</li>
+                                <li><span>Phone:</span>445656446464</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="contact-form">
+                        <h4>Get in Touch</h4>
+                        <div className="form">
+                            <form>
+                                <input name="name" type="text" className="rtnsinp" placeholder="Name"/>
+                                <input name="email" type="text" className="rtnsinp" placeholder="Email"/>
+                                <input name="phone" type="text" className="rtnsinp" placeholder="Phone"/>
+                                <textarea name="message" className="rtnsinp" placeholder="Message"></textarea>
+                                <button className="submit" type="submit">Send Message</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div className="contact-form">
-            <h4>Get in Touch</h4>
-            <div className="form">
-                <form>
-                    <input name="name" type="text" className="rtnsinp" placeholder="Name"/>
-                    <input name="email" type="text" className="rtnsinp" placeholder="Email"/>
-                    <input name="phone" type="text" className="rtnsinp" placeholder="Phone"/>
-                    <textarea name="message" className="rtnsinp" placeholder="Message"></textarea>
-                    <button className="submit" type="submit">Send Message</button>
-                </form>
-            </div>
-        </div>
-      </div>
     )
   }
 }
